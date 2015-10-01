fa84bcc6ab86b75a8a73d893d3d7e2943ab50f72
Leonardo Almeida Araújo <leo@leoalmar.com>
2015-10-01 17:37:31 -0300

    link logo

--- resources/views/site/partials/header.blade.php
+++ resources/views/site/partials/header.blade.php
@@ -8,7 +8,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">My Laravel Blog</a>
+			<a class="navbar-brand" ui-sref="home">My Laravel Blog</a>
 		</div>
 
 		
